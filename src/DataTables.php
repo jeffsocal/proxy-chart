46caa24440b9439b8a9b89115ed890a162b8f595
Jeff Jones <jeff@socalbioinformatics.com>
2018-04-20 08:50:12 -0700

    moved html content to proxy-html and extended to charts with js implementation

---
+++ src/DataTables.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * Written by Jeff Jones (jeff@socalbioinformatics.com)
+ * Copyright (2016) SoCal Bioinformatics Inc.
+ *
+ * See LICENSE.txt for the license.
+ */
+namespace ProxyChart;
+
+use ProxyHTML\Tables;
+
+class DataTables extends Tables
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function getImports()
+    {
+        return 'https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js';
+    }
+
+    public function getTableJS()
+    {
+        return '<script>' . "
+                    $(document).ready( function () {
+                        $('#" . $this->table_id . "').DataTable();
+                    } );
+                " . '</script>' . PHP_EOL;
+    }
+}
+?>
